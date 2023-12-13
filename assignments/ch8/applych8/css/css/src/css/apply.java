052fcc299934656bbc0556d0ffb39c6ba7b6cce0
Veekss <145696249+Veekss@users.noreply.github.com>
2023-12-13 13:49:16 -0500

    Add files via upload

---
+++ assignments/ch8/applych8/css/css/src/css/apply.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package css;
+
+/**
+ * 
+ */
+public class apply {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
